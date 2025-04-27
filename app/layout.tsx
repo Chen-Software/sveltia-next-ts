ffd40460e076326ffbf7dd40ae29e31c446a898c
Yumin Chen <yumin@chen.so>
2025-04-27 09:02:13 +0100

    Remove Netlify Identity (#17)
    ---
    
    - Issue: https://git.chen.so/sveltia-next-ts/i/7525f7709d811b0179a036882979716ef068d93a

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import '../styles/prism-a11y-dark.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { AnimatePresence } from 'framer-motion'
-import NetlifyIdentityRedirect from '../components/NetlifyIdentityRedirect'
 
 export default function RootLayout({
   children,
@@ -17,10 +16,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <script
-          async
-          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-        />
       </head>
       <body className="flex flex-col h-[100vh] text-slate-900 dark:text-slate-50 dark:bg-slate-900 hyphens-auto">
         <Header />
@@ -32,7 +27,6 @@ export default function RootLayout({
           {children}
         </AnimatePresence>
         <Footer />
-        <NetlifyIdentityRedirect />
       </body>
     </html>
   )
